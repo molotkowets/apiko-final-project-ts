d3bbc340f92990ea5d79334f09c29643c1d249cf
molotkowets <45715581+molotkowets@users.noreply.github.com>
2023-10-04 19:43:52 +0300

    Revert "header - 'not authorized'"

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
 import './App.css';
-import './styles/main-styles.css'
-import Home from './pages/home/Home';
-import Layout from './components/layout/Layout';
-
 
 function App() {
   return (
-    <>
-      <Routes>
-          <Route path='/' element={<Layout/>}>
-            <Route index element={<Home/>}/>
-          </Route>
-      </Routes>
-    </>
+    <div className="App">
+      
+    </div>
   );
 }
 
 export default App;
-
-
