22d1f6fdb860e5d97af13d8b60d38732c50c0a84
molotkowets <molotkowets@gmail.com>
2023-12-26 14:43:24 +0200

    isn`t delete

--- src/middleware/query-product-by-id.ts
+++ src/middleware/query-product-by-id.ts
@@ -1,18 +1,18 @@
-import { useQuery, type DefaultError } from "@tanstack/react-query";
-import ProductsAPI, { type GetProductResponse } from "../services/ProductsAPI";
+// import { useQuery, type DefaultError } from "@tanstack/react-query";
+// import ProductsAPI, { type GetProductResponse } from "../services/ProductsAPI";
 
 export const GetProductsByIds = (id: string): any => {
-    const { data, isLoading, fetchStatus, error } = useQuery<
-        string | GetProductResponse,
-        DefaultError,
-        string | GetProductResponse,
-        [string, string]
-    >({
-        queryKey: ["products", id],
-        queryFn: async ({ queryKey: [, _id] }) => {
-            console.log("test");
-            return await ProductsAPI.byId(id);
-        },
-    });
-    return { data, isLoading, fetchStatus, error };
+    // const { data, isLoading, fetchStatus, error } = useQuery<
+    //     string | GetProductResponse,
+    //     DefaultError,
+    //     string | GetProductResponse,
+    //     [string, string]
+    // >({
+    //     queryKey: ["products", id],
+    //     queryFn: async ({ queryKey: [, _id] }) => {
+    //         console.log("test");
+    //         return await ProductsAPI.byId(id);
+    //     },
+    // });
+    // return { data, isLoading, fetchStatus, error };
 };
